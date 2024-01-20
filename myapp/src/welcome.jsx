3fc991dad4cc24a7eb19c4cf509b6b67d66f084a
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2024-01-20 11:22:02 +0100

    Add files via upload

---
+++ myapp/src/welcome.jsx
@@ -0,0 +1,13 @@
+export function Welcome({name}){
+    return <p>Welcome, {name}!</p>
+}
+/* 1)Nel caso in cui non si passi alcuna" prop" name al componente ,verrà renderizzato solo il 
+messaggio nel componente function Welcome senza il nome, dunque il messaggio che appare a
+sarà "Welcome,!"  
+2)Sì, puoi impostare un valore predefinito (default) per una prop in React.
+ Nel componente Welcome, si può utilizzare una "default props" per specificare u
+n valore per la prop "name".
+Welcome.defaultProps = {
+  name: 'Guest',
+};
+*/
